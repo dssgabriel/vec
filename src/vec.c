cb2e65b135ded34eeedb66949c842c610482849f
dssgabriel <dss.gabriel@protonmail.com>
2021-08-22 03:18:35 +0200

    Finished reworking functions and cleaned up code

--- src/vec.c
+++ src/vec.c
@@ -1,333 +1,324 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 
 #include "vec.h"
 
-static const size_t type_size[10] = {
-    sizeof(uint8_t),
-    sizeof(int8_t),
-    sizeof(uint16_t),
-    sizeof(int16_t),
-    sizeof(uint32_t),
-    sizeof(int32_t),
-    sizeof(uint64_t),
-    sizeof(int64_t),
-    sizeof(float),
-    sizeof(double)
-};
+static inline
+void* vec_offset(vec_t* self, size_t offset) {
+    return self->data + offset * self->item_size;
+}
 
+inline
 void vec_drop(vec_t* self) {
     free(self->data);
     free(self);
 }
 
-vec_t* vec_new(T data_type) {
+vec_t* vec_new(size_t item_size) {
     vec_t* v = malloc(sizeof(vec_t));
 
-    if (v) {
-        v->len = 0;
-        v->capacity = 1;
-        v->data_type = data_type;
-
-        if (data_type > U8 && data_type <= F64) {
-            v->item_size = type_size[data_type];
-        }
-
-        v->data = malloc(v->item_size);
-
-        if (v->data) {
-            return v;
-        } else {
-            return NULL;
-        }
-    } else {
+    if (!v) {
         return NULL;
     }
+
+    v->len = 0;
+    v->capacity = 0;
+    v->item_size = item_size;
+    v->data = NULL;
+
+    return v;
 }
 
-vec_t* vec_with_capacity(T data_type, size_t capacity) {
-    vec_t* v = vec_new(data_type);
+vec_t* vec_with_capacity(size_t capacity, size_t item_size) {
+    vec_t* v = malloc(sizeof(vec_t));
 
-    if (v) {
-        v->capacity = capacity;
-        v->data = malloc(capacity * v->item_size);
-
-        if (v->data) {
-            return v;
-        } else {
-            return NULL;
-        }
-    } else {
+    if (!v) {
         return NULL;
     }
+
+    v->len = 0;
+    v->capacity = capacity;
+    v->item_size = item_size;
+    v->data = malloc(capacity * item_size);
+
+    if (!v->data) {
+        return NULL;
+    }
+
+    return v;
 }
 
-vec_t* vec_from_raw_parts(T data_type, void* raw_ptr, size_t len) {
-    vec_t* v = vec_new(data_type);
+vec_t* vec_from_raw_parts(void* raw_ptr, size_t len, size_t item_size) {
+    vec_t* v = vec_with_capacity(len, item_size);
 
-    if (v) {
-        if (raw_ptr) {
-            v->len = len;
-        } else {
-            v->len = 0;
-        }
-
-        v->capacity = len;
-        v->data = raw_ptr;
-
-        return v;
-    } else {
+    if (!v) {
         return NULL;
     }
+
+    v->data = raw_ptr;
+
+    return v;
 }
 
 int vec_copy(vec_t* self, vec_t* other) {
     if (!self || !other) {
-        return -1;
+        return VEC_ERR;
     }
 
     other->len = self->len;
     other->capacity = self->capacity;
-    other->item_size = self->item_size;
 
     free(other->data);
     other->data = malloc(self->item_size * self->capacity);
 
-    if (other->data) {
-        memcpy(other->data, self->data, self->item_size * self->capacity);
-        return 0;
-    } else {
-        return -2;
+    if (!other->data) {
+        return VEC_ERR;
     }
+
+    memcpy(other->data, self->data, self->item_size * self->capacity);
+    
+    return VEC_OK;
 }
 
+inline
 int vec_is_empty(vec_t* self) {
-    if (self) {
-        return self->len == 0 ? 1 : 0;
-    } else {
-        return -1;
+    if (!self) {
+        return VEC_ERR;
     }
+
+    return self->len == 0 ? 1 : 0;
 }
 
-void* vec_peak_front(vec_t* self) {
-    if (self->data) {
-        return &self->data[0];
-    } else {
-        return NULL;
-    }
-}
-
-void* vec_peak_back(vec_t* self) {
-    if (self->data) {
-        return &self->data[self->len - 1];
-    } else {
-        return NULL;
-    }
-}
-
-void* vec_peak_at(vec_t* self, size_t index) {
-    if (index < self->len) {
-        if (self->data) {
-            return &self->data[index];
-        } else {
-            return NULL;
-        }
-    } else {
+void* vec_peak(vec_t* self, size_t index) {
+    if (index >= self->len) {
         printf("Error: `index` (%lu) is out of bounds", index);
         vec_drop(self);
         exit(-1);
     }
+    
+    if (!self->data) {
+        return NULL;
+    }
+
+    return vec_offset(self, index);
 }
 
 int vec_resize(vec_t* self, size_t new_capacity) {
-    if (self) {
-        if (self->data) {
-            if (self->capacity > new_capacity) {
-                self->len = new_capacity;
-            }
-
-            self->capacity = new_capacity;
-            self->data = realloc(self->data, new_capacity * self->item_size);
-
-            return (self->data != NULL) ? 0 : -3;
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
+    if (!self || !self->data || self->len > new_capacity) {
+        return VEC_ERR;
     }
+
+    if (self->capacity >= new_capacity) {
+        return VEC_OK;
+    }
+
+    self->capacity = new_capacity;
+    self->data = realloc(self->data, new_capacity * self->item_size);
+
+    return self->data ? VEC_OK : VEC_ERR;
 }
 
-int vec_reserve_by(vec_t* self, size_t amount) {
-    if (self) {
-        if (self->data) {
-            self->capacity += amount;
-            self->data = realloc(self->data, self->capacity * self->item_size);
-
-            return (self->data != NULL) ? 0 : -3;
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
+int vec_reserve(vec_t* self, size_t amount) {
+    if (!self || !self->data) {
+        return VEC_ERR;
     }
-}
 
-int vec_reserve_to(vec_t* self, size_t new_capacity) {
-    if (self) {
-        if (self->data) {
-            if (self->capacity < new_capacity) {
-                self->capacity = new_capacity;
-                self->data = realloc(self->data, self->capacity * self->item_size);
+    self->capacity += amount;
+    self->data = realloc(self->data, self->capacity * self->item_size);
 
-                return (self->data != NULL) ? 0 : -3;
-            } else {
-                return -4;
-            }
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
-    }
-}
-
-int vec_shrink_by(vec_t* self, size_t amount) {
-    if (self) {
-        if (self->data) {
-            self->capacity -= amount;
-            self->data = realloc(self->data, self->capacity * self->item_size);
-
-            return (self->data != NULL) ? 0 : -3;
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
-    }
-}
-
-int vec_shrink_to(vec_t* self, size_t new_capacity) {
-    if (self) {
-        if (self->data) {
-            if (self->capacity > new_capacity) {
-                self->capacity = new_capacity;
-                self->data = realloc(self->data, self->capacity * self->item_size);
-
-                return (self->data != NULL) ? 0 : -3;
-            } else {
-                return -4;
-            }
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
-    }
+    return self->data ? VEC_OK : VEC_ERR;
 }
 
 int vec_shrink_to_fit(vec_t* self) {
-    if (self) {
-        if (self->data) {
-            self->capacity = self->len;
-            self->data = realloc(self->data, self->capacity * self->item_size);
-
-            return (self->data != NULL) ? 0 : -3;
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
+    if (!self || !self->data) {
+        return VEC_ERR;
     }
+
+    self->capacity = self->len;
+    self->data = realloc(self->data, self->capacity * self->item_size);
+
+    return self->data ? VEC_OK : VEC_ERR;
 }
 
 int vec_clear(vec_t* self) {
-    if (self) {
-        self->len = 0;
-        self->capacity = 0;
-        free(self->data);
-
-        return 0;
-    } else {
-        return -1;
+    if (!self) {
+        return VEC_ERR;
     }
+
+    self->len = 0;
+    self->capacity = 0;
+    free(self->data);
+
+    return VEC_OK;
 }
 
 int vec_push(vec_t* self, void* item) {
-    if (self) {
-        if (self->data) {
-            if (self->capacity == self->len) {
-                int ret = vec_resize(self, self->capacity * 2);
-
-                if (!ret) {
-                    return -2;
-                }
-            }
-
-            void* offset = self->data + self->len;
-            memcpy(offset, item, self->item_size);
-            self->len += 1;
-
-            return 0;
-        } else {
-            self->data = malloc(self->item_size);
-
-            if (self->data) {
-                memcpy(self->data, item, self->item_size);
-
-                return 0;
-            } else {
-                return -3;
-            }
-        }
-    } else {
-        return -1;
+    if (!self) {
+        return VEC_ERR;
     }
+
+    if (!self->data) {
+        self->capacity = 2;
+        self->data = malloc(self->capacity * self->item_size);
+
+        if (!self->data) {
+            return VEC_ERR;
+        }
+    }
+
+    if (self->len == self->capacity) {
+        int ret = vec_resize(self, self->capacity * 2);
+
+        if (!ret) {
+            return VEC_ERR;
+        }
+    }
+
+    void* ptr = vec_offset(self, self->len);
+    memcpy(ptr, item, self->item_size);
+    self->len += 1;
+
+    return VEC_OK;
 }
 
-// int vec_insert(vec_t* self, void* item, size_t index) {}
-
-// TODO: Needs checking
-void* vec_pop(vec_t* self) {
-    if (self) {
-        if (self->data) {
-            void* offset = self->data + self->len - self->item_size;
-            void* value;
-            memcpy(value, offset, self->item_size);
-            offset = NULL;
-            self->len -= 1;
-
-            return value;
-        } else {
-            return NULL;
-        }
-    } else {
-        return NULL;
+int vec_insert(vec_t* self, void* item, size_t index) {
+    if (index >= self->len) {
+        printf("Error: `index` (%lu) is out of bounds", index);
+        vec_drop(self);
+        exit(-1);
     }
+
+    if (!self || !self->data) {
+        return VEC_ERR;
+    }
+
+    if (self->len == self->capacity) {
+        int ret = vec_reserve(self, 1);
+
+        if (!ret) {
+            return VEC_ERR;
+        }
+    }
+
+    void* ptr = vec_offset(self, index);
+    
+    memcpy(ptr + self->item_size, ptr, (self->len - index) * self->item_size);
+    memcpy(ptr, item, self->item_size);
+    self->len += 1;
+
+    return VEC_OK;
 }
 
-// int vec_remove(vec_t* self, size_t index) {}
+int vec_pop(vec_t* self, void* ret) {
+    if (!self || !self->data || vec_is_empty(self)) {
+        return VEC_ERR;
+    }
+    
+    void* ptr = vec_offset(self, self->len - 1);
 
-// TODO: Needs checking
+    memcpy(ret, ptr, self->item_size);
+    self->len -= 1;
+
+    return VEC_OK;
+}
+
+int vec_remove(vec_t* self, size_t index) {
+    if (index >= self->len) {
+        printf("Error: `index` (%lu) is out of bounds", index);
+        vec_drop(self);
+        exit(-1);
+    }
+
+    if (!self || !self->data) {
+        return VEC_ERR;
+    }
+
+    void* ptr = vec_offset(self, index);
+
+    memcpy(ptr, ptr + self->item_size, (self->len - index) * self->item_size);
+    self->len -= 1;
+
+    return VEC_OK;
+}
+
 int vec_swap_remove(vec_t* self, size_t index) {
-    if (self) {
-        if (self->data) {
-            if (index < self->len) {
-                void* value;
-                void* swap = self->data + self->len - self->item_size;
-                memcpy(value, swap, self->item_size);
-                swap = NULL;
-
-                return 0;
-            } else {
-                return -3;
-            }
-        } else {
-            return -2;
-        }
-    } else {
-        return -1;
+    if (index >= self->len) {
+        printf("Error: `index` (%lu) is out of bounds", index);
+        vec_drop(self);
+        exit(-1);
     }
+
+    if (!self || !self->data) {
+        return VEC_ERR;
+    }
+
+    void* ptr = vec_offset(self, index);
+    void* last = vec_offset(self, self->len - 1);
+
+    memcpy(ptr, last, self->item_size);
+    self->len -= 1;
+
+    return VEC_OK;
+}
+
+int vec_append(vec_t* self, vec_t* other) {
+    if (!self || !other || !self->data || !other->data) {
+        return VEC_ERR;
+    }
+
+    if (self->len + other->len > self->capacity) {
+        int ret = vec_reserve(self, other->len);
+
+        if (!ret) {
+            return VEC_ERR;
+        }
+    }
+
+    void* ptr = vec_offset(self, self->len);
+    memcpy(ptr, other->data, other->len * other->item_size);
+    self->len += other->len;
+    vec_clear(other);
+
+    return VEC_OK;
+}
+
+int vec_split_at(vec_t* self, vec_t* other, size_t index) {
+    if (index >= self->len) {
+        printf("Error: `index` (%lu) is out of bounds", index);
+        vec_drop(self);
+        exit(-1);
+    }
+
+    if (!self || !other || !self->data) {
+        return VEC_ERR;
+    }
+
+    if (!other->data || other->len < self->len - index) {
+        int ret = vec_reserve(other, (self->len - index));
+
+        if (!ret) {
+            return VEC_ERR;
+        }
+    }
+
+    void* ptr = vec_offset(self, index);
+    memcpy(other->data, ptr, (self->len - index) * self->item_size);
+    other->len = self->len - index;
+    self->len = index;
+
+    return VEC_OK;
+}
+
+void vec_print(vec_t* self) {
+    if (!self || !self->data || vec_is_empty(self)) {
+        printf("[ ]\n");
+        return;
+    }
+
+    printf("[");
+    for (size_t i = 0; i < self->len - 1; i++) {
+        printf("%d, ", ((int*)self->data)[i]);
+    }
+    printf("%d]\n", ((int*)self->data)[self->len - 1]);
 }
--- src/vec.h
+++ src/vec.h
@@ -1,57 +1,41 @@
-#pragma once
+#ifndef VEC_H
+#define VEC_H
 
 #include <stddef.h>
 #include <stdint.h>
 
-typedef enum : uint16_t {
-    U8, I8, U16, I16, U32, I32, U64, I64, F32, F64
-} T;
+#define VEC_OK  1
+#define VEC_ERR 0
 
 typedef struct vec_s {
     size_t len;
     size_t capacity;
-    uint8_t item_size;
-    T data_type;
+    size_t item_size;
     void* data;
 } vec_t;
 
-// Allocations
-vec_t* vec_new(T data_type);
-vec_t* vec_with_capacity(T data_type, size_t capacity);
-vec_t* vec_from_raw_parts(T data_type, void* raw_ptr, size_t len);
+void vec_drop(vec_t* self);
+vec_t* vec_new(size_t item_size);
+vec_t* vec_with_capacity(size_t capacity, size_t item_size);
+vec_t* vec_from_raw_parts(void* raw_ptr, size_t len, size_t item_size);
 int vec_copy(vec_t* self, vec_t* other);
 
-// Informations
 int vec_is_empty(vec_t* self);
-void* vec_peak_front(vec_t* self);
-void* vec_peak_back(vec_t* self);
-void* vec_peak_at(vec_t* self, size_t index);
+void* vec_peak(vec_t* self, size_t index);
 
-// Memory management
 int vec_resize(vec_t* self, size_t new_capacity);
-
-int vec_reserve_by(vec_t* self, size_t amount);
-int vec_reserve_to(vec_t* self, size_t new_capacity);
-
-int vec_shrink_by(vec_t* self, size_t amount);
-int vec_shrink_to(vec_t* self, size_t new_capacity);
+int vec_reserve(vec_t* self, size_t amount);
 int vec_shrink_to_fit(vec_t* self);
+int vec_clear(vec_t* self);
 
-int vec_clear(vec_t*);
-
-// Mutating
 int vec_push(vec_t* self, void* item);
 int vec_insert(vec_t* self, void* item, size_t index);
-
-void* vec_pop(vec_t* self);
+int vec_pop(vec_t* self, void* ret);
 int vec_remove(vec_t* self, size_t index);
 int vec_swap_remove(vec_t* self, size_t index);
-
 int vec_append(vec_t* self, vec_t* other);
 int vec_split_at(vec_t* self, vec_t* other, size_t index);
 
-// Misc
-int vec_sort(vec_t* self);
+void vec_print(vec_t* self);
 
-// De-allocation
-void vec_drop(vec_t* self);
+#endif
